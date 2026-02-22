03ffa2df1756f35b33d12a908b8fc1dee6739d90
Manan Vyas <mavyas@cotality.com>
2026-02-22 19:53:02 +1000

    Spring-boot

---
+++ src/main/java/myproject/github_action_test/GithubActionTestApplication.java
@@ -0,0 +1,13 @@
+package myproject.github_action_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionTestApplication.class, args);
+	}
+
+}
